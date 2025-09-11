a81e83f4617df5e278885e81716e89b9361716c0
wtx <2185321603@qq.com>
2025-09-11 19:17:10 +0800

    个人博客项目001:任务4完成

--- src/main/java/com/wtx/myblog/controller/ArticleController.java
+++ src/main/java/com/wtx/myblog/controller/ArticleController.java
@@ -7,8 +7,6 @@ import com.wtx.myblog.utils.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
